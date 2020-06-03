e3c6f75defc9cf96c2a09914ca3634dc194ba917
gheorghe.cazacu@icloud.com <qRV-BXk-hPX-h9T>
2020-06-03 21:11:40 +0300

    add some functionalities in ProductService added some details in README.md

---
+++ spring-mysql-docker/README.md
@@ -0,0 +1,28 @@
+# spring mysql docker project
+
+###Purpose
+The purpose of this project is to give an idea how you ca develop a spring boot application
+with a database but without installing on it on the local machine.
+
+### Prerequisite 
+A version of docker installed locally and functional
+Knowledge of java and gradle
+
+First step is to build the application from Gradle window
+
+just press gradle button and select clean build
+from terminal 
+
+docker-compose up --build -d
+
+for stop in terminal
+
+docker-compose stop
+
+testing url
+
+http://localhost:8181/product/4
+
+
+
+![print-screen-terminal.png](./documents/print-screen-terminal.png)
--- spring-mysql-docker/web/src/main/java/com/clyde/tutorial/services/ProductService.java
+++ spring-mysql-docker/web/src/main/java/com/clyde/tutorial/services/ProductService.java
@@ -1,17 +1,28 @@
 package com.clyde.tutorial.services;
 
 import com.clyde.tutorial.dto.Product;
+import com.clyde.tutorial.persistence.ProductEntity;
+import com.clyde.tutorial.persistence.ProductRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
 
+    private ProductRepository productRepository;
+    private ModelMapper modelMapper = new ModelMapper();
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     public Product getProduct(Long id) {
-        Product result = new Product();
-        result.setId(id);
-        result.setName("name");
-        result.setCategory("category");
-        return result;
+        Optional<ProductEntity> result = productRepository.findById(id);
+        return result.isPresent() ? modelMapper.map(result.get(), Product.class) : null;
     }
 
 }
